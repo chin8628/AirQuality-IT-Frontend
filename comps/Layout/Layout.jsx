63c2c90de3118e664866d236bf74135dc9a1b0a0
chin8628 <boonyarith.pir@gmail.com>
2019-04-05 19:00:28 +0700

    Updated title

--- comps/Layout/Layout.jsx
+++ comps/Layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
-      <title>AQI - IT@KMITL</title>
+      <title>IT@KMITL - Air Quality Monitor</title>
     </Head>
     <style jsx global>
       {`
